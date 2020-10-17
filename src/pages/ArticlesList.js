e0dba710d377e02b3f7860a63f7882a501da35b6
Demetrio <demetrioalmeida@gmail.com>
2020-10-17 00:48:32 -0400

    fetch articles

--- src/pages/ArticlesList.js
+++ src/pages/ArticlesList.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import articleContent from "./article-content";
 
 const ArticlesList = () => {
   return (
     <div>
-      <h1>Articles</h1>
+      {articleContent.map((article, key) => (
+        <Link
+          to={`/article/${article.name}`}
+          key={key}
+          className='article-list-item'
+        >
+          <h3>{article.title}</h3>
+          <p>{article.content[0].substring(0, 150)}</p>
+        </Link>
+      ))}
     </div>
   );
 };
